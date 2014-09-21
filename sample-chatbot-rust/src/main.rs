d8f69ff96f7d08ab704dee666dcd09c473633b29
Igor Khomenko <igor@quickblox.com>
2014-09-21 16:40:21 +0300

    connect to chat implementation

--- sample-chatbot-rust/src/main.rs
+++ sample-chatbot-rust/src/main.rs
@@ -1,3 +1,20 @@
+extern crate xmpp;
+use xmpp::XmppStream;
+
 fn main() {
-    println!("Hello, world!")
+    
+    let mut stream = XmppStream::new("1608427-92", "chat.quickblox.com", "rustuser");
+    
+    println!("Connetiing...");
+
+    match stream.connect() {
+        Ok(_) => {
+            println!("Connected");
+        },
+        Err(e) => {
+            println!("Error while connecting: {}", e);
+            return;
+        }
+    }
+    println!("{}", stream.handle());
 }
